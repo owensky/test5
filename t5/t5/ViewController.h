0185b7df0af635a75c14b5efd370b8224e486393
owensky <owen8708@163.com>
2016-07-01 17:28:58 +0800

    1

---
+++ t5/t5/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  t5
+//
+//  Created by 闫永超 on 16/7/1.
+//  Copyright © 2016年 闫永超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ t5/t5/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  t5
+//
+//  Created by 闫永超 on 16/7/1.
+//  Copyright © 2016年 闫永超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
